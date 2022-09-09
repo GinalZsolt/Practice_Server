51f6ec5e4b7856dc87a73a3fa9dfc9cd7aaefbdf
ginalzsolt <ginalzsolt@turr.hu>
2022-09-09 10:27:29 +0200

    kész

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require('express'), path = require('path'),app= express();
 
 app.listen(3000)
-
-app.use('body-parse')
+app.use('/css',express.static(path.join(__dirname,'./css')))
+app.use('/img',express.static(path.join(__dirname,'./img')))
+app.use(express.urlencoded({extended:true}))
 
 app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname,'./html/me'))
+    res.sendFile(path.join(__dirname,'./html/me.html'))
 })
 
 app.get('/hobbies', (req,res)=>{
-    res.sendFile(path.join(__dirname,'./html/me'))
+    res.sendFile(path.join(__dirname,'./html/hobby.html'))
 })
 
 app.get('/contacts', (req,res)=>{
-    res.sendFile(path.join(__dirname,'./html/me'))
+    res.sendFile(path.join(__dirname,'./html/contacts.html'))
+})
+
+app.post('/uzenet', (req,res)=>{
+    console.log(req.body)
+    res.send('Kész!')
 })
